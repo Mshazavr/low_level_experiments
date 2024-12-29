ffe3bf33f71a494efc3c0c81a7de6bbd40b1d8c2
Ubuntu <ubuntu@ip-172-31-16-235.us-east-2.compute.internal>
2024-12-29 17:17:25 +0000

    avx512vl info

--- utils/avx.hpp
+++ utils/avx.hpp
@@ -20,14 +20,16 @@ void print_extension_availability() {
     _debug("%s", "+-----------+-----------+");
     _debug("%s", "| Extension | Supported |");
     _debug("%s", "+-----------+-----------+");
-    _debug(      "| sse       | %s|", __builtin_cpu_supports("sse")     ? "yes       " : "no        ");
+    _debug(      "| sse       | %s|", __builtin_cpu_supports("sse")      ? "yes       " : "no        ");
     _debug("%s", "+-----------+-----------+");
-    _debug(      "| sse2      | %s|", __builtin_cpu_supports("sse2")    ? "yes       " : "no        ");
+    _debug(      "| sse2      | %s|", __builtin_cpu_supports("sse2")     ? "yes       " : "no        ");
     _debug("%s", "+-----------+-----------+");
-    _debug(      "| avx       | %s|", __builtin_cpu_supports("avx")     ? "yes       " : "no        ");
+    _debug(      "| avx       | %s|", __builtin_cpu_supports("avx")      ? "yes       " : "no        ");
     _debug("%s", "+-----------+-----------+");
-    _debug(      "| avx2      | %s|", __builtin_cpu_supports("avx2")    ? "yes       " : "no        ");
+    _debug(      "| avx2      | %s|", __builtin_cpu_supports("avx2")     ? "yes       " : "no        ");
     _debug("%s", "+-----------+-----------+");
-    _debug(      "| avx512f   | %s|", __builtin_cpu_supports("avx512f") ? "yes       " : "no        ");
+    _debug(      "| avx512f   | %s|", __builtin_cpu_supports("avx512f")  ? "yes       " : "no        ");
+    _debug("%s", "+-----------+-----------+");
+    _debug(      "| avx512vl  | %s|", __builtin_cpu_supports("avx512vl") ? "yes       " : "no        ");
     _debug("%s", "+-----------+-----------+");
 }
